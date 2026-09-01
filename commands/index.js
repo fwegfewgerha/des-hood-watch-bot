131d2125213a4b74c04399eabb9cac3f2b0af46c
fwegfewgerha <steelogreen058@gmail.com>
2026-09-01 09:01:47 -0400

    Create index.js

---
+++ commands/index.js
@@ -0,0 +1,7 @@
+const embed = require('./embed');
+const stats = require('./stats');
+const raidprotect = require('./raidprotect');
+const secure = require('./secure');
+const modlog = require('./modlog');
+
+module.exports = [embed, stats, raidprotect, secure, modlog];
